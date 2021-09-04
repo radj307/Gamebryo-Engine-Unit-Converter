41dab9e457ffbd975daba220785dbeb383b44539
radj307 <radj@hotmail.ca>
2021-09-04 19:44:22 -0400

    Added proper help display

--- Gamebryo-Conv/build-cfg.hpp
+++ Gamebryo-Conv/build-cfg.hpp
@@ -4,149 +4,151 @@
 #include <INI_Defaults.hpp>
 #include <strconv.hpp>
 
-struct {
-	const section_map
-		// Default Setting Values
-		defMap{
-			{
-				"conversions", {
-					{ "imperial_to_metric",	"0.3048" },			// Factor_MI
-					{ "metric_to_unit",		"0.0142875313" },	// Factor_UM
-					{ "imperial_to_unit",	"0.046875" },	// Factor_UI
-				}
-			},
-			{
-				"config", {
-					{ "precision", "6" },
-					{ "disable-colors", "false" },
-					{ "always-show-input", "false" },
-				}
-			},
-	},
-		// Default Setting Comments
-		defCommentMap{
-			{
-				"conversions", {
-					{ "imperial_to_metric",	"Used for conversions between metric & imperial. ([Metric] = [Imperial] * [Factor])" },
-					{ "metric_to_unit",		"Used for conversions between metric & units. ([Units] = [Metric] * [Factor])" },
-					{ "imperial_to_unit",	"Used for conversions between imperial & units. ([Units] = [Imperial] * [Factor])" },
-				}
-			},
-			{
-				"config", {
-					{ "disable-colors", "false" },
-					{ "always-show-input", "false" },
-				}
-			},
+namespace ck_unit_conv {
+	struct {
+		const section_map
+			// Default Setting Values
+			defMap{
+				{
+					"conversions", {
+						{ "imperial_to_metric",	"0.3048" },			// Factor_MI
+						{ "metric_to_unit",		"0.0142875313" },	// Factor_UM
+						{ "imperial_to_unit",	"0.046875" },	// Factor_UI
+					}
+				},
+				{
+					"config", {
+						{ "precision", "6" },
+						{ "disable-colors", "false" },
+						{ "always-show-input", "false" },
+					}
+				},
+		},
+			// Default Setting Comments
+			defCommentMap{
+				{
+					"conversions", {
+						{ "imperial_to_metric",	"Used for conversions between metric & imperial. ([Metric] = [Imperial] * [Factor])" },
+						{ "metric_to_unit",		"Used for conversions between metric & units. ([Units] = [Metric] * [Factor])" },
+						{ "imperial_to_unit",	"Used for conversions between imperial & units. ([Units] = [Imperial] * [Factor])" },
+					}
+				},
+				{
+					"config", {
+						{ "disable-colors", "false" },
+						{ "always-show-input", "false" },
+					}
+				},
+		};
+
+		bool write(const std::string& filename, bool append = false)
+		{
+			return INI_Defaults{ defMap, defCommentMap }.write(filename, append);
+		}
+
+		INI buildINI()
+		{
+			return INI( std::forward<decltype(defMap)>(defMap) );
+		}
+
+	} DEFAULT_INI;
+
+	/**
+	 * @class Config
+	 * @brief Wrapper for the INI class used for config values throughout the conversion program.
+	 */
+	class Config {
+		INI* _config{ nullptr }; ///< @brief Pointer to an INI instance, used by all member functions to retrieve values.
+
+	public:
+		/**
+		 * Config() = delete;
+		 * @brief Deleted default constructor.
+		 */
+		Config() = delete;
+
+		/**
+		 * Config(INI&)
+		 * @brief Constructor that takes a reference to an INI instance.
+		 * @param config_inst	- Reference to an INI instance.
+		 */
+		explicit Config(INI& config_inst) noexcept : _config{ &config_inst } {}
+
+		/**
+		 * get(string&&)
+		 * @brief Get a pointer to a specified header in the INI file.
+		 * @param header	- Target INI header.
+		 * @returns INI::key_list*
+		 * @throws exception("CONFIG_INI_NULLPTR")	- The local INI instance pointer was nullptr.
+		 */
+		INI::key_list* get(std::string&& header)
+		{
+			if ( _config != nullptr ) return _config->getSecPtr(std::forward<std::string>(header));
+			throw std::exception("CONFIG_INI_NULLPTR");
+		}
+
+		/**
+		 * get(string&&, string&&)
+		 * @brief Get the value of a specified INI config value.
+		 * @param header	- The name of the header containing the target key.
+		 * @param key		- The name of the target key.
+		 * @returns string
+		 * @throws exception("CONFIG_INI_NULLPTR")	- The local INI instance pointer was nullptr.
+		 */
+		std::string get(std::string&& header, std::string&& key)
+		{
+			if ( _config != nullptr ) return _config->get(std::forward<std::string>(header), std::forward<std::string>(key)).value_or("");
+			throw std::exception("CONFIG_INI_NULLPTR");
+		}
+
+		/**
+		 * iGet(string&&, string&&)
+		 * @brief Get the value of a specified INI config value, as a long.
+		 * @param header	- The name of the header containing the target key.
+		 * @param key		- The name of the target key.
+		 * @returns long
+		 */
+		long iGet(std::string&& header, std::string&& key)
+		{
+			return str::stol(get(std::forward<std::string>(header), std::forward<std::string>(key)));
+		}
+
+		/**
+		 * fGet(string&&, string&&)
+		 * @brief Get the value of a specified INI config value, as a double.
+		 * @param header	- The name of the header containing the target key.
+		 * @param key		- The name of the target key.
+		 * @returns double
+		 */
+		double fGet(std::string&& header, std::string&& key)
+		{
+			return str::stod(get(std::forward<std::string>(header), std::forward<std::string>(key)));
+		}
+
+		/**
+		 * bGet(string&&, string&&)
+		 * @brief Get the value of a specified INI config value, as a boolean.
+		 * @param header	- The name of the header containing the target key.
+		 * @param key		- The name of the target key.
+		 * @returns bool
+		 */
+		bool bGet(std::string&& header, std::string&& key)
+		{
+			return str::stob(get(std::forward<std::string>(header), std::forward<std::string>(key)));
+		}
+
+		/**
+		 * operator<<(ostream&, const Config&)
+		 * @brief Stream insertion operator.
+		 * @param os	- (Auto) Target output stream reference.
+		 * @param c		- (Auto) Target config reference.
+		 * @returns ostream&
+		 */
+		friend std::ostream& operator<<(std::ostream& os, const Config& c) noexcept
+		{
+			os << c._config;
+			return os;
+		}
 	};
-
-	bool write(const std::string& filename, bool append = false)
-	{
-		return INI_Defaults{ defMap, defCommentMap }.write(filename, append);
-	}
-
-	INI buildINI()
-	{
-		return INI( std::forward<decltype(defMap)>(defMap) );
-	}
-
-} DEFAULT_INI;
-
-/**
- * @class Config
- * @brief Wrapper for the INI class used for config values throughout the conversion program.
- */
-class Config {
-	INI* _config{ nullptr }; ///< @brief Pointer to an INI instance, used by all member functions to retrieve values.
-
-public:
-	/**
-	 * Config() = delete;
-	 * @brief Deleted default constructor.
-	 */
-	Config() = delete;
-
-	/**
-	 * Config(INI&)
-	 * @brief Constructor that takes a reference to an INI instance.
-	 * @param config_inst	- Reference to an INI instance.
-	 */
-	explicit Config(INI& config_inst) noexcept : _config{ &config_inst } {}
-
-	/**
-	 * get(string&&)
-	 * @brief Get a pointer to a specified header in the INI file.
-	 * @param header	- Target INI header.
-	 * @returns INI::key_list*
-	 * @throws exception("CONFIG_INI_NULLPTR")	- The local INI instance pointer was nullptr.
-	 */
-	INI::key_list* get(std::string&& header)
-	{
-		if ( _config != nullptr ) return _config->getSecPtr(std::forward<std::string>(header));
-		throw std::exception("CONFIG_INI_NULLPTR");
-	}
-
-	/**
-	 * get(string&&, string&&)
-	 * @brief Get the value of a specified INI config value.
-	 * @param header	- The name of the header containing the target key.
-	 * @param key		- The name of the target key.
-	 * @returns string
-	 * @throws exception("CONFIG_INI_NULLPTR")	- The local INI instance pointer was nullptr.
-	 */
-	std::string get(std::string&& header, std::string&& key)
-	{
-		if ( _config != nullptr ) return _config->get(std::forward<std::string>(header), std::forward<std::string>(key)).value_or("");
-		throw std::exception("CONFIG_INI_NULLPTR");
-	}
-
-	/**
-	 * iGet(string&&, string&&)
-	 * @brief Get the value of a specified INI config value, as a long.
-	 * @param header	- The name of the header containing the target key.
-	 * @param key		- The name of the target key.
-	 * @returns long
-	 */
-	long iGet(std::string&& header, std::string&& key)
-	{
-		return str::stol(get(std::forward<std::string>(header), std::forward<std::string>(key)));
-	}
-
-	/**
-	 * fGet(string&&, string&&)
-	 * @brief Get the value of a specified INI config value, as a double.
-	 * @param header	- The name of the header containing the target key.
-	 * @param key		- The name of the target key.
-	 * @returns double
-	 */
-	double fGet(std::string&& header, std::string&& key)
-	{
-		return str::stod(get(std::forward<std::string>(header), std::forward<std::string>(key)));
-	}
-
-	/**
-	 * bGet(string&&, string&&)
-	 * @brief Get the value of a specified INI config value, as a boolean.
-	 * @param header	- The name of the header containing the target key.
-	 * @param key		- The name of the target key.
-	 * @returns bool
-	 */
-	bool bGet(std::string&& header, std::string&& key)
-	{
-		return str::stob(get(std::forward<std::string>(header), std::forward<std::string>(key)));
-	}
-
-	/**
-	 * operator<<(ostream&, const Config&)
-	 * @brief Stream insertion operator.
-	 * @param os	- (Auto) Target output stream reference.
-	 * @param c		- (Auto) Target config reference.
-	 * @returns ostream&
-	 */
-	friend std::ostream& operator<<(std::ostream& os, const Config& c) noexcept
-	{
-		os << c._config;
-		return os;
-	}
-};
-Config* cfg{ nullptr }; ///< @brief This is required until I go through the whole program and change the use of global vars.
+	Config* cfg{ nullptr }; ///< @brief This is required until I go through the whole program and change the use of global vars.
+}
--- Gamebryo-Conv/main.cpp
+++ Gamebryo-Conv/main.cpp
@@ -3,17 +3,23 @@
 #include <resolve-path.hpp>
 
 #include "build-cfg.hpp"
-
 #include "interpret.hpp"
+#include "Help.hpp"
 
 int main(const int argc, char** argv, char** envp)
 {
+	using namespace ck_unit_conv;
+
 	const std::string DEFAULT_INI_NAME{ "Gamebryo-Converter.ini"};
-	const opt::Param args(argc, argv, { { }, { { "file", true }, { "reset-ini", false } } });
+	const opt::Param args(argc, argv, { { 'h' }, { { "help", false }, {"file", true}, {"reset-ini", false}, {"ini-reset", false}}});
+
+	const auto [location, name] { opt::resolve_split_path(envp, argv[0]) };
+
+	if ( args.empty() || args.flag('h') || args.check_opt("help") )
+		std::cout << Help(name, 32) << '\n';
 
 	INI ini;
-	const auto resetINI{ args.check_opt("resetini") };
-
+	const auto resetINI{ args.check_opt("reset-ini") || args.check_opt("ini-reset")};
 
 	if ( resetINI ) { // reset INI
 		if ( DEFAULT_INI.write(DEFAULT_INI_NAME) )
@@ -24,7 +30,7 @@ int main(const int argc, char** argv, char** envp)
 
 	if ( file::exists(DEFAULT_INI_NAME) )
 		ini.read(DEFAULT_INI_NAME); // read INI file if it exists
-	else if ( const auto [location, name] { opt::resolve_split_path(envp, argv[0]) }; file::exists(location + DEFAULT_INI_NAME) )
+	else if ( file::exists(location + DEFAULT_INI_NAME) )
 		ini.read(location + DEFAULT_INI_NAME); // read ini on PATH if it exists
 	else // use default ini if none found.
 		ini = std::move(DEFAULT_INI.buildINI());
