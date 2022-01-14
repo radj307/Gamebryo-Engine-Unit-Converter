ad7577b72a234532012e8cd4f419e425ff74d7c2
radj307 <radj307@gmail.com>
2022-01-14 16:24:35 -0500

    Add basic cmake project

---
+++ ckconv/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <TermAPI.hpp>
+
+int main(const int argc, char** argv)
+{
+	std::cout << term::EnableANSI;
+	try {
+
+
+		return 0;
+	} catch (const std::exception& ex) {
+		std::cerr << term::error << ex.what() << std::endl;
+		return -1;
+	}catch (...){
+		std::cerr << term::crit << "An unknown exception occurred!" << std::endl;
+		return -1;
+	}
+}
