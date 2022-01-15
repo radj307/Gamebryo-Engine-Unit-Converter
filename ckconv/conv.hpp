095c6248e1b5dcada23235e71d8a38f0b4e5697e
radj307 <radj307@gmail.com>
2022-01-15 09:30:24 -0500

    implement most of build-5

---
+++ ckconv/conv.hpp
@@ -0,0 +1,252 @@
+/**
+ * @file	unit.hpp
+ * @author	radj307
+ * @brief	Contains real-world measurement unit converters, currently supporting metric & imperial.
+ */
+#include <sysarch.h>
+#include <make_exception.hpp>
+#include <str.hpp>
+#include <var.hpp>
+#include <math.hpp>
+#include <TermAPI.hpp>
+
+#include <optional>
+#include <algorithm>
+
+namespace ckconv {
+	/**
+	 * @enum	System
+	 * @brief	Accepted Measurement Systems
+	 */
+	enum class System : char {
+		METRIC,
+		IMPERIAL,
+		CREATIONKIT,
+	};
+
+	/**
+	 * @struct	Metric
+	 * @brief	Intra-Metric-System Conversion Factors
+	 */
+	static struct { // System::METRIC
+		enum class MetricPowers : char {
+			PICOMETER = -12,
+			NANOMETER = -9,
+			MICROMETER = -6,
+			MILLIMETER = -3,
+			CENTIMETER = -2,
+			DECIMETER = -1,
+			METER = 0,
+			DECAMETER = 1,
+			HECTOMETER = 2,
+			KILOMETER = 3,
+			MEGAMETER = 6,
+			GIGAMETER = 9,
+			TERAMETER = 12,
+		};
+
+		using T = long double;
+
+		const T PICOMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::PICOMETER)) };
+		const T NANOMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::NANOMETER)) };
+		const T MICROMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::MICROMETER)) };
+		const T MILLIMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::MILLIMETER)) };
+		const T CENTIMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::CENTIMETER)) };
+		const T DECIMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::DECIMETER)) };
+		const T METER{ std::pow(10.0l, static_cast<long double>(MetricPowers::METER)) };
+		const T DECAMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::DECAMETER)) };
+		const T HECTOMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::HECTOMETER)) };
+		const T KILOMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::KILOMETER)) };
+		const T MEGAMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::MEGAMETER)) };
+		const T GIGAMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::GIGAMETER)) };
+		const T TERAMETER{ std::pow(10.0l, static_cast<long double>(MetricPowers::TERAMETER)) };
+
+		const T* base{ &METER };
+	} Metric;
+
+	/**
+	 * @struct	Imperial
+	 * @brief	Intra-Imperial-System Conversion Factors
+	 */
+	static struct { // System::IMPERIAL
+		using T = long double;
+
+		const T INCH{ 1.0l / 12.0l };
+		const T FOOT{ 1.0l };
+		const T YARD{ 3.0l };
+
+		const T* base{ &FOOT };
+	} Imperial;
+
+	static struct { // System::CREATIONKIT
+		using T = long double;
+		const T UNIT{ 1.0l };
+
+		const T* base{ &UNIT };
+	} CreationKit;
+
+	/// @brief	Inter-System (Metric:Imperial) Conversion Factor
+	const constexpr auto ONE_FOOT_IN_METERS{ 0.3048 };
+	/// @brief	Inter-System (CKUnit:Metric) Conversion Factor
+	const constexpr auto ONE_UNIT_IN_METERS{ 0.01428 }, ONE_UNIT_IN_FEET{ 0.046875 };
+
+	/**
+	 * @struct	Unit
+	 * @brief	POD Structure with System & conversion factor members used to represent a measurement unit.
+	 */
+	struct Unit {
+		System system;
+		long double unitcf;
+		//	Unit(System sys, long double dbl) : system{ sys }, unitcf{ dbl } {}
+		long double base(const long double& val) const
+		{
+			return val * unitcf;
+		}
+		CONSTEXPR bool operator==(const Unit& o) const
+		{
+			return system == o.system && unitcf == o.unitcf;
+		}
+		CONSTEXPR operator System() const { return system; }
+		CONSTEXPR operator long double() const { return unitcf; }
+	};
+
+	/**
+	 * @brief			Converts between units in one measurement system.
+	 * @param in_unit	Input Conversion Factor
+	 * @param v			Input Value
+	 * @param out_unit	Output Conversion Factor
+	 * @returns			long double
+	 */
+	inline constexpr long double convert_unit(const long double& in_unit, const long double& v, const long double& out_unit)
+	{
+		return ((v * in_unit) / out_unit);
+	}
+	/**
+	 * @brief				Convert between measurement systems.
+	 * @param in_system		Input Measurement System
+	 * @param v_base		Input Value, in the input system's base unit. (Metric = Meters, Imperial = Feet)
+	 * @param out_system	Output Measurement System
+	 * @returns				long double
+	 */
+	inline constexpr long double convert_system(const System& in_system, const long double& v_base, const System& out_system)
+	{
+		if (in_system == out_system) // same system
+			return v_base;
+		// convert system
+		switch (in_system) {
+		case System::METRIC:
+			switch (out_system) { // METRIC ->
+			case System::IMPERIAL:
+				return v_base / ONE_FOOT_IN_METERS;
+			case System::CREATIONKIT:
+				return v_base / ONE_UNIT_IN_METERS;
+			}
+			break;
+		case System::IMPERIAL:
+			switch (out_system) { // IMPERIAL ->
+			case System::METRIC:
+				return v_base * ONE_FOOT_IN_METERS;
+			case System::CREATIONKIT:
+				return v_base / ONE_UNIT_IN_FEET;
+			}
+			break;
+		case System::CREATIONKIT:
+			switch (out_system) { // CREATIONKIT ->
+			case System::METRIC:
+				return v_base * ONE_UNIT_IN_METERS;
+			case System::IMPERIAL:
+				return v_base * ONE_UNIT_IN_FEET;
+			}
+			break;
+		default:break;
+		}
+		throw make_exception("convert_system() failed:  No handler exists for the given input type!");
+	}
+
+	/**
+	 * @brief		Convert a number in a given unit to another unit and/or system.
+	 * @param in	Input Unit.
+	 * @param val	Input Value.
+	 * @param out	Output Unit.
+	 * @returns		long double
+	 */
+	template<var::arithmetic T>
+	inline static constexpr long double convert(const Unit& in, const T& val, const Unit& out)
+	{
+		if (in.unitcf == 0.0l)
+			throw make_exception("Illegal input conversion factor");
+		if (out.unitcf == 0.0l)
+			throw make_exception("Illegal output conversion factor");
+
+		if (in.system == out.system) // convert between units only
+			return convert_unit(in.unitcf, val, out.unitcf);
+		// Convert between systems & units
+		return convert_system(in.system, in.base(static_cast<long double>(val)), out.system) / out.unitcf;
+	}
+
+	/**
+	 * @brief		Retrieve the unit specified by a string containing the unit's official symbol, or name.
+	 * @param str	Input String. (This is not processed beyond case-conversion)
+	 * @param def	Optional default return value if the string is invalid.
+	 * @returns		Unit
+	 */
+	inline Unit getUnit(const std::string& str, const std::optional<Unit>& def = std::nullopt)
+	{
+		if (str.empty()) {
+			if (def.has_value())
+				return def.value();
+			throw make_exception("No unit specified ; string was empty!");
+		}
+		const auto s{ str::tolower(str) };
+
+		// check imperial
+		// negative
+		if (str == "in" || s == "i" || str::matches_min_any<false>(s, 4, "inch", "inches"))
+			return Unit{ System::IMPERIAL, Imperial.INCH };
+		// positive
+		if (str == "yd" || s == "y" || str::matches_min_any<false>(s, 4, "yard", "yards"))
+			return Unit{ System::IMPERIAL, Imperial.YARD };
+		// base
+		if (str == "ft" || s == "f" || str::matches_min_any<false>(s, 4, "foot", "feet"))
+			return Unit{ System::IMPERIAL, Imperial.FOOT };
+
+		// check metric (str == (symbol) || str == (name))
+		// negative
+		if (str == "pm" || str::matches_min_any<false>(s, 7, "picometer"))
+			return Unit{ System::METRIC, Metric.PICOMETER };
+		if (str == "nm" || str::matches_min_any<false>(s, 7, "nanometer"))
+			return Unit{ System::METRIC, Metric.NANOMETER };
+		if (str == "µm" || str == "um" || str::matches_min_any<false>(s, 8, "micrometer"))
+			return Unit{ System::METRIC, Metric.MICROMETER };
+		if (str == "mm" || str::matches_min_any<false>(s, 8, "millimeter"))
+			return Unit{ System::METRIC, Metric.MILLIMETER };
+		if (str == "cm" || str::matches_min_any<false>(s, 8, "centimeter"))
+			return Unit{ System::METRIC, Metric.CENTIMETER };
+		if (str == "dm" || str::matches_min_any<false>(s, 7, "decimeter"))
+			return Unit{ System::METRIC, Metric.DECIMETER };
+		// positive
+		if (str == "dam" || str::matches_min_any<false>(s, 7, "decameter"))
+			return Unit{ System::METRIC, Metric.DECAMETER };
+		if (str == "hm" || str::matches_min_any<false>(s, 8, "hectometer"))
+			return Unit{ System::METRIC, Metric.HECTOMETER };
+		if (str == "km" || str::matches_min_any<false>(s, 7, "kilometer"))
+			return Unit{ System::METRIC, Metric.KILOMETER };
+		if (str == "Mm" || str::matches_min_any<false>(s, 7, "megameter"))
+			return Unit{ System::METRIC, Metric.MEGAMETER };
+		if (str == "Gm" || str::matches_min_any<false>(s, 7, "gigameter"))
+			return Unit{ System::METRIC, Metric.GIGAMETER };
+		if (str == "Tm" || str::matches_min_any<false>(s, 7, "terameter"))
+			return Unit{ System::METRIC, Metric.TERAMETER };
+		// base
+		if (str == "m" || str::matches_min_any<false>(s, 3, "meter"))
+			return Unit{ System::METRIC, Metric.METER };
+
+		if (str == "u" || str == "un" || str::matches_min_any<false>(s, 4, "unit"))
+			return Unit{ System::CREATIONKIT, CreationKit.UNIT };
+
+		if (def.has_value())
+			return def.value();
+
+		throw make_exception("Unrecognized Unit: \"", str, '\"');
+	}
+}
--- ckconv/main.cpp
+++ ckconv/main.cpp
@@ -1,17 +1,152 @@
-﻿#include <TermAPI.hpp>
+#include "conv.hpp"
+#include "Global.h"
+
+#include <envpath.hpp>
+
+/**
+ * @struct	Convert
+ * @brief	Performs a single conversion operation, and exposes std::ostream operator<<() to format and insert it into an output stream.
+ */
+struct Convert {
+	/// @brief	String Tuple
+	using Tuple = std::tuple<std::string, std::string, std::string>;
+private:
+	Tuple _vars;
+
+	///	@brief	Sorts the first & second arguments so that they are in the correct order when passed to the converter. Also removes any commas.
+	static inline Tuple&& sort_args(Tuple&& tpl)
+	{
+		auto& [first, second, third] { tpl };
+
+		// swap the first & second args if the first argument is the value
+		if (std::all_of(first.begin(), first.end(), [](auto&& ch) { return isdigit(ch) || ch == '.' || ch == '-' || ch == ','; })) {
+			const auto copy{ second };
+			second = str::strip(first, ',');
+			first = copy;
+		}
+		else second = str::strip(second, ',');
+
+		return std::move(tpl);
+	}
+
+	///	@brief	Returns the result of the conversion
+	long double getResult() const noexcept(false) { return ckconv::convert(ckconv::getUnit(std::get<0>(_vars)), str::stold(std::get<1>(_vars)), ckconv::getUnit(std::get<2>(_vars))); }
+
+public:
+	/// @brief	Default constructor
+	Convert(std::tuple<std::string, std::string, std::string>&& vars) : _vars{ sort_args(std::move(vars)) } {}
+	/**
+	 * @brief			Constructor
+	 * @param unit_in	Input Unit (OR Input Value, if val_in is the input unit)
+	 * @param val_in	Input Value (OR Input Unit, if unit_in is the input value)
+	 * @param unit_out	Output Unit
+	*/
+	Convert(const std::string& unit_in, const std::string& val_in, const std::string& unit_out) : _vars{ sort_args(std::make_tuple(unit_in, val_in, unit_out)) } {}
+
+	long double operator()() const { return getResult(); }
+
+	/**
+	 * @brief	Format and print the result of the conversion to the given ostream instance.
+	 *\n		This may change the floatfield and/or precision of the output stream.
+	 * @returns	std::ostream&
+	 */
+	friend std::ostream& operator<<(std::ostream& os, const Convert& conv)
+	{
+		const auto& [unit_in, input, unit_out] {conv._vars};
+		const auto output{ conv.getResult() };
+
+		// override floatfield notation
+		if (Global.notation != nullptr)
+			os.setf(*Global.notation);
+
+		// modify precision
+		if (Global.precision != 6ll)
+			os.precision(Global.precision);
+
+		// quiet
+		if (!Global.quiet) {
+			os  // show input value & unit
+				<< Global.palette.set(OUT::INPUT_VALUE) << input << Global.palette.reset()
+				<< ' '
+				<< Global.palette.set(OUT::INPUT_UNIT) << unit_in << Global.palette.reset()
+				<< Global.palette.set(OUT::EQUALS) << " = " << Global.palette.reset();
+		}
+
+
+		return os // output:
+			<< Global.palette.set(OUT::OUTPUT_VALUE) << output << Global.palette.reset()
+			<< ' '
+			<< Global.palette.set(OUT::OUTPUT_UNIT) << unit_out << Global.palette.reset()
+			;
+	}
+};
 
 int main(const int argc, char** argv)
 {
-	std::cout << term::EnableANSI;
 	try {
+		const auto clog_rdbuf{ std::clog.rdbuf(Global.log.rdbuf()) };
 
+		// parse arguments
+		opt::ParamsAPI2 args{ argc, argv, 'p', "precision" };
+		// find the program's location
+		const auto [program_path, program_name] { env::PATH().resolve_split(argv[0]) };
+
+		// handle help argument
+		if (args.check_any<opt::Flag, opt::Option>('h', "help")) {
+			write_help(std::cout, program_name.generic_string());
+			return 0;
+		}
+		// handle version argument
+		else if (args.check_any<opt::Flag, opt::Option>('v', "version")) {
+			std::cout << program_name.generic_string() << " v" << ckconv_VERSION << std::endl;
+			return 0;
+		}
+
+		// handle other arguments
+		handle_args(args);
+
+		// Set the ini path
+		Global.ini_path = [&program_path, &program_name]() {
+			std::filesystem::path tmp{ program_name };
+			tmp.replace_extension("ini");
+			return program_path / tmp;
+		}();
+
+		// read the ini if it exists
+		if (file::exists(Global.ini_path))
+			handle_config(file::INI(Global.ini_path));
+
+		const auto params{ args.typegetv_all<opt::Parameter>() };
+
+		const auto& getArgTuple{ [&params](decltype(params)::const_iterator& it) -> Convert::Tuple {
+			const auto first{ *it };
+			const auto second{ *++it };
+			const auto third{ *++it };
+			return{ first, second, third };
+		} };
+
+		for (auto it{ params.begin() }; it < params.end(); ++it) {
+			if (std::distance(it, params.end()) > 2ull) {
+				std::cout << "  " << Convert(getArgTuple(it)) << '\n';
+			}
+		}
 
 		return 0;
 	} catch (const std::exception& ex) {
 		std::cerr << term::error << ex.what() << std::endl;
 		return -1;
-	}catch (...){
-		std::cerr << term::crit << "An unknown exception occurred!" << std::endl;
+	} catch (...) {
+		std::cerr << term::crit << "An undefined exception occurred!" << std::endl;
 		return -1;
 	}
+
+
+	std::cout << Convert("u", "1", "m") << '\n';
+	std::cout << Convert("u", "99", "m") << '\n';
+	std::cout << Convert("m", "1", "u") << '\n';
+	std::cout << Convert("m", "1", "ft") << '\n';
+	std::cout << Convert("mm", "500", "u") << '\n';
+	std::cout << Convert("mm", "500", "in") << '\n';
+
+	return 0;
 }
